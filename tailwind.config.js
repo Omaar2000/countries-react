c45e9285de22631fe5640af95198770270f41d54
unknown <omarcasio@gmail.com>
2025-03-04 16:39:27 +0200

    Initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure Tailwind scans the correct files
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
